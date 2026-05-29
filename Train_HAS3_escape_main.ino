61f1bc9aaac92420f372b9511177c2ff3bb0ca4e
Hyohyoju <julie08051@naver.com>
2026-05-29 11:18:50 +0900

    Firmware Update v14

---
+++ Train_HAS3_escape_main.ino
@@ -0,0 +1,68 @@
+ /**
+ * @file Train_HAS3_escape_main.ino
+ * @author 김병준 (you@domain.com)
+ * @brief
+ * @version 1.0
+ * @date 2022-11-29
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#define FIRMWARE_VER 14
+#include "Train_HAS3_escape_main.h"
+
+void setup() {
+    delay(500);
+    Serial.begin(115200);
+    toSubSerial.begin(115200, SERIAL_8N1, HWSERIAL_RX, HWSERIAL_TX);
+//    has2wifi.Setup("city");
+    has2wifi.Setup("badland_shoot", "Has3");
+    ota.setLogStream(DebugSerial);
+    ota.setOnSuccess([]() {
+        has2wifi.Send((String)(const char*)my["device_name"], "device_state", "setting");
+    });
+    ota.setOnSkip([]() {
+        has2wifi.Send((String)(const char*)my["device_name"], "device_state", "setting");
+    });
+    TelnetInit();
+    NeopixelInit();
+    TimerInit();
+    Mp3_Setup();
+    StepMotorInit();
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, HIGH);
+    // has2wifi.Setup("KT_GiGA_6C64","ed46zx1198");
+    // has2wifi.Setup();
+    DataChanged();
+    toSubSerial.println("R");
+    toSubSerial.println("R");
+    toSubSerial.println("R");
+
+    // QC 엔진 초기화 및 룰 등록
+    // QCEngine& qc = QCEngine::getInstance();
+    // qc.begin(2000);
+    // qc.addRule(new QCRule_HeapMemory());
+    // qc.addRule(new QCRule_WifiConnection());
+    // qc.addRule(new QCRule_WifiSignal());
+    // qc.addRule(new QCRule_ResetReason());
+    // qc.addRule(new QCRule_PinConflict());
+    // qc.addRule(new QCRule_GpioCapability());
+    // qc.addRule(new QCRule_GameState());
+    // qc.addRule(new QCRule_TagCntBounds());
+    // qc.addRule(new QCRule_StepperConfig());
+    // qc.addRule(new QCRule_LimitSwitch());
+    // qc.addRule(new QCRule_RelayState());
+    // qc.addRule(new QCRule_SwitchChatter());      // HW_SW_03
+    // qc.addRule(new QCRule_BeetlePacketFormat()); // LOGIC_SERIAL_02
+    // qc.addRule(new QCRule_InvalidCommand());     // LOGIC_SERIAL_03
+    // qc.addRule(new QCRule_TagParseError());      // LOGIC_TAG_02
+    // qc.addRule(new QCRule_StrappingPins());      // HW_BOOT_01
+    // qc.addRule(new QCRule_OutputInitSafety());   // HW_GPIO_02
+}
+void loop() {
+    TelnetRun();
+    WifiTimer.run();
+    GameTimer.run();
+    //QCEngine::getInstance().tick();
+}
